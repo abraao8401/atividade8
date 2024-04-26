24daddd1426e5059bc15390c21405f151ec68a39
Abraao Santiago Moreira <santiagoabraao17@gmail.com>
2024-04-26 17:14:15 -0300

    commit 4

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -1,3 +1,22 @@
 public class ContaBancaria {
-    
+    private Double saldo;
+
+    public ContaBancaria() {
+        this.saldo = 0.0;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(Double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(Double valor) throws SaldoInsuficienteException {
+        if (valor > saldo) {
+            throw new SaldoInsuficienteException("Saldo insuficiente para realizar o saque.");
+        }
+        saldo -= valor;
+    }
 }
