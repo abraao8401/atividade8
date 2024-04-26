73cb6f8374339512437894140e9864b74329c2dc
Abraao Santiago Moreira <santiagoabraao17@gmail.com>
2024-04-26 16:55:33 -0300

    commit 2

--- Vetor.java
+++ Vetor.java
@@ -1,3 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Vetor {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] vetor = new int[10];
+        int indice = 0;
+
+        try {
+            while (true) {
+                System.out.print("Digite um valor inteiro (ou 0 para sair): ");
+                int valor = scanner.nextInt();
+                
+                if (valor == 0) {
+                    vetor[indice] = valor;
+                    break;
+                }
+
+                if (indice == 10) {
+                    throw new ArrayIndexOutOfBoundsException("Você inseriu mais que 10 valores.");
+                }
+
+                vetor[indice] = valor;
+                indice++;
+            }
+
+            System.out.println("Valores digitados:");
+            for (int i = 0; i < indice; i++) {
+                System.out.println(vetor[i]);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Valor informado não é numérico.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Erro: Você inseriu mais que 10 valores.");
+        } finally {
+            scanner.close();
+        }
+    }
 }
